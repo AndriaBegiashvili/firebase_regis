fc1c084803b5cb6150dfee36251e5e7bb888f826
AndriaBegiashvili <andria.begiashvili@gmail.com>
2021-12-08 01:31:32 +0400

    123

--- app/src/main/java/com/example/registrationfire/MainActivity.kt
+++ app/src/main/java/com/example/registrationfire/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.core.text.isDigitsOnly
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthEmailException
 import com.google.firebase.ktx.Firebase
 
 
@@ -15,11 +16,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editPass1 : TextView
     private lateinit var editPass2 : TextView
     private lateinit var button : Button
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
-        registerlis()
+        registerList()
     }
     private fun init(){
         editEmail = findViewById(R.id.editEmail)
@@ -28,27 +30,45 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.button)
     }
 
-    private fun registerlis() {
+    private fun registerList() {
         button.setOnClickListener {
             val editEm = editEmail.text.toString()
             val editPas = editPass1.text.toString()
             val editPas1 = editPass2.text.toString()
 
-            if (editEm.isEmpty() || '@' !in editEm || '.' !in editEm){
-                editEmail.error = "Enter correct E-mail"
+
+            if (!(editEm.matches(emailPattern.toRegex()))){
+                editEmail.error = "Incorrect Email"
                 return@setOnClickListener
             }
-            else if (editPas.count()<8 || editPas.isEmpty()){
+            if (editPas.count()<9 || editPas.isEmpty()){
                 editPass1.error = "Enter Password"
                 return@setOnClickListener
             }
-            else if (editPas1 != editPas){
+            if(editPas.isDigitsOnly()){
+                editPass1.error = "Must contain letters"
+                return@setOnClickListener
+            }
+            if (!(editPas.matches(".*[A-Z].*".toRegex())) && !(editPas.matches(".*[a-z].*".toRegex())) ){
+                editPass1.error = "Must contain letters"
+                return@setOnClickListener
+            }
+            if (!(editPas.matches(".*[0-9].*".toRegex()))){
+                editPass1.error = "Must contain digits"
+                return@setOnClickListener
+            }
+            if (!(editPas.matches(".*[$@#!?_].*".toRegex()))){
+                editPass1.error = "Must contain special symbols '$@#!?_'"
+            }
+            if (editPas1 != editPas){
                 editPass2.error = "Passwords don't match"
                 return@setOnClickListener
             }
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(editEm,editPas).addOnCompleteListener{ task-> if(task.isSuccessful){
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(editEm,editPas).addOnCompleteListener{ task-> if(task.isSuccessful) {
                 Toast.makeText(this, "You registered successfully", Toast.LENGTH_SHORT).show()
-            } }
+            }
+            else Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show()}
+
 
 
 
